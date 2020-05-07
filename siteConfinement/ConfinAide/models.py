9c6bc358fd2c3a8abd920430a3ddd3fff6d50024
LeoDPlouc <leo.lambert02@gmail.com>
2020-05-07 15:26:06 +0200

    Created client un models

--- siteConfinement/ConfinAide/models.py
+++ siteConfinement/ConfinAide/models.py
@@ -1,16 +1,9 @@
 from django.db import models
 
-class Customer(models.Model):
-    pass
-
-class OrderService(models.Model):
-    pass
-
-class Service(models.Model):
-    pass
-
-class OrderProduit(models.Model):
-    pass
-
-class Produit(models.Model):
-    pass
+class Client(models.Model):
+    id_client = models.BigIntegerField(primary_key=True)
+    password = models.CharField(max_length = 20)
+    mail = models.CharField(max_length = 50)
+    nom = models.CharField(max_length = 20, null = True)
+    tel = models.CharField(max_length = 20, null = True)
+    adresse = models.CharField(max_length = 20, null = True)
